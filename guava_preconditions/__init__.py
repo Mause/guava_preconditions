c16263d8e69a7613222c370b2b5d3de1686816b9
Elliana <me@mause.me>
2021-10-09 19:37:41 +0800

    Remove duplicate declarations

--- guava_preconditions/__init__.py
+++ guava_preconditions/__init__.py
@@ -1,34 +1,16 @@
-from typing import Optional, TypeVar
+from typing import Any, Optional, TypeVar
 
 T = TypeVar("T")
 
 
-def checkArgument(expression: bool):
+def checkArgument(
+    expression: bool, errorMessageTemplate: Optional[str] = None, *errorMessageArgs: Any
+) -> None:
     "Ensures the truth of an expression involving one or more parameters to the calling method."
 
 
-def checkArgument(expression: bool, errorMessage: Any):
-    "Ensures the truth of an expression involving one or more parameters to the calling method."
-
-
-def checkArgument(expression: bool, errorMessageTemplate: str, *errorMessageArgs: Any):
-    "Ensures the truth of an expression involving one or more parameters to the calling method."
-
-
-def checkElementIndex(index: int, size: int) -> int:
+def checkElementIndex(index: int, size: int, desc: str = None) -> int:
     "Ensures that index specifies a valid element in an array, list or string of size size."
-
-
-def checkElementIndex(index: int, size: int, desc: str) -> int:
-    "Ensures that index specifies a valid element in an array, list or string of size size."
-
-
-def checkNotNull(reference: Optional[T]) -> T:
-    "Ensures that an object reference passed as a parameter to the calling method is not null."
-
-
-def checkNotNull(reference: Optional[T], errorMessage: Any) -> T:
-    "Ensures that an object reference passed as a parameter to the calling method is not null."
 
 
 def checkNotNull(
@@ -37,25 +19,15 @@ def checkNotNull(
     "Ensures that an object reference passed as a parameter to the calling method is not null."
 
 
-def checkPositionIndex(index: int, size: int) -> int:
+def checkPositionIndex(index: int, size: int, desc: str = None) -> int:
     "Ensures that index specifies a valid position in an array, list or string of size size."
 
 
-def checkPositionIndex(index: int, size: int, desc: str) -> int:
-    "Ensures that index specifies a valid position in an array, list or string of size size."
-
-
-def checkPositionIndexes(start: int, end: int, size: int):
+def checkPositionIndexes(start: int, end: int, size: int) -> None:
     "Ensures that start and end specify a valid positions in an array, list or string of size size, and are in order."
 
 
-def checkState(expression: bool):
-    "Ensures the truth of an expression involving the state of the calling instance, but not involving any parameters to the calling method."
-
-
-def checkState(expression: bool, errorMessage: Any):
-    "Ensures the truth of an expression involving the state of the calling instance, but not involving any parameters to the calling method."
-
-
-def checkState(expression: bool, errorMessageTemplate: str, *errorMessageArgs: Any):
+def checkState(
+    expression: bool, errorMessageTemplate: str = None, *errorMessageArgs: Any
+) -> None:
     "Ensures the truth of an expression involving the state of the calling instance, but not involving any parameters to the calling method."
